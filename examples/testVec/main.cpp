4a7eb1f9cef9e7b7f080a4c3bb8e548cb061d156
Sascha Kaden <sascha.kaden@s2012.tu-chemnitz.de>
2016-05-22 22:47:47 +0200

    add vec test example

---
+++ examples/testVec/main.cpp
@@ -0,0 +1,44 @@
+#include <cstdint>
+#include <iostream>
+
+#include <memory>
+#include <ctime>
+#include <Vec.h>
+#include <Node.h>
+#include <Graph.h>
+#include <Sampling.h>
+
+int main(int argc, char** argv)
+{
+    const unsigned int dim = 2;
+
+    float temp = 10.0;
+    Vec<float> vec1(temp,temp);
+    Vec<float> vec2(vec1);
+    vec2[1] = 11;
+
+    Vec<float> vec6(2,6,3,6);
+    vec6[0] = 3;
+
+    Vec<float> vec3(2,4);
+
+    vec3 = vec1 + vec2;
+
+    //Vec<double> vecx = vec2 + vec6;
+    Node node(vec1);
+    Graph graph;
+    graph.addNode(std::shared_ptr<Node>(new Node(vec1)));
+    graph.addNode(std::shared_ptr<Node>(new Node(vec2)));
+    graph.addNode(std::shared_ptr<Node>(new Node(vec3)));
+
+    Vec<float> vecX(15,15);
+    graph.getNearestNode(vecX);
+    Sampling samp;
+    Vec<float> minBoundary(0.0,0.0);
+    Vec<float> maxBoundary(1000.0,1000.0);
+    samp.setBoundaries(minBoundary, maxBoundary);
+    samp.getSample(2, 5, 10);
+
+
+    return 0;
+}
