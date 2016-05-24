ca3607bb55a568ebf7a82682d0fe55c75afdab71
Sascha Kaden <sascha.kaden@s2012.tu-chemnitz.de>
2016-05-24 21:50:35 +0200

    update docu to Node

---
+++ html/search/functions_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['clearchilds',['clearChilds',['../classNode.html#a3c2f953a0a08323110014c5fcb8def22',1,'Node']]],
+  ['clearparent',['clearParent',['../classNode.html#a6c4496fb0c1cca593766765f6878f0e3',1,'Node']]]
+];
---
+++ html/search/functions_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['empty',['empty',['../classNode.html#abfe17e7be0efb32246a6fffad333ed9a',1,'Node']]]
+];
