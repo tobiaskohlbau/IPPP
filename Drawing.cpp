25b9c76a1221846d068dc48ae1e4a81e6e4dda36
Tobias Kohlbau <tobias.kohlbau@gmail.com>
2016-05-30 12:59:46 +0200

    Seperates header into cpp and headers
    Signed-off-by: Tobias Kohlbau <tobias.kohlbau@gmail.com>

---
+++ Drawing.cpp
@@ -0,0 +1,42 @@
+#include "Drawing.h"
+
+/*!
+*  \brief          Draw nodes and there edge to the parent Node
+*  \author         Sascha Kaden
+*  \param[in]      nodes
+*  \param[in, out] image
+*  \date           2016-05-25
+*/
+void Drawing::drawTree(const std::vector<shared_ptr<Node>> &nodes, cv::Mat &image, const Vec<uint8_t> &colorNode, const Vec<uint8_t> &colorEdge, const int &thickness)
+{
+    assert(nodes[0]->getDim() == 2);
+    for (auto& elem : nodes) {
+        cv::Point point(elem->getX(), elem->getY());
+        cv::circle(image, point, 3, cv::Scalar(colorNode[0], colorNode[1], colorNode[2]), 2);
+        if (elem->getParent() != nullptr) {
+            cv::Point point2(elem->getParent()->getX(), elem->getParent()->getY());
+            cv::line(image, point, point2, cv::Scalar(colorEdge[0], colorEdge[1], colorEdge[2]), thickness);
+        }
+    }
+}
+
+/*!
+*  \brief          Draw a path from the goal Node to the init Node
+*  \author         Sascha Kaden
+*  \param[in]      goal Node
+*  \param[in, out] image
+*  \date           2016-05-25
+*/
+void Drawing::drawPath(const shared_ptr<Node> &goalNode, cv::Mat &image, const Vec<uint8_t> &colorNode, const Vec<uint8_t> &colorEdge, const int &thickness)
+{
+    assert(goalNode->getDim() == 2);
+    // build vector for Drawing
+    std::vector<shared_ptr<Node>> nodes;
+    nodes.push_back(goalNode);
+    shared_ptr<Node> temp = goalNode->getParent();
+    while(temp != nullptr) {
+        nodes.push_back(temp);
+        temp = temp->getParent();
+    }
+    drawTree(nodes, image, colorNode, colorEdge, thickness);
+}
---
+++ Sampling.cpp
@@ -0,0 +1,71 @@
+#include "Sampling.h"
+
+/*!
+*  \brief      Constructor of the class Sampling
+*  \author     Sascha Kaden
+*  \param[in]  SamplingMethod
+*  \date       2016-05-24
+*/
+Sampling::Sampling(const SamplingMethod &method) {
+    m_method = method;
+    srand(time(NULL));
+}
+
+/*!
+*  \brief      Return sample
+*  \author     Sascha Kaden
+*  \param[in]  dimension
+*  \param[in]  index from loop of samples
+*  \param[in]  absolute number of samples, which will be created
+*  \param[out] Vec
+*  \date       2016-05-24
+*/
+Vec<float> Sampling::getSample(const unsigned int &dim, const int index, const int nbSamples) {
+    Vec<float> vec(dim);
+    if (!checkBoudaries())
+        return vec;
+
+    if (true)//m_method == SamplingMethod::randomly)
+    {
+        for (unsigned int i = 0; i < dim; ++i)
+            vec[i] = m_minBoundary[i] + (float)(rand() % (int)(m_maxBoundary[i]-m_minBoundary[i]));
+        return vec;
+    }
+    return vec;
+}
+
+/*!
+*  \brief      Set minimum and maximum boundaries of configuration space
+*  \author     Sascha Kaden
+*  \param[in]  minimum boundaries
+*  \param[in]  maximum boundaries
+*  \date       2016-05-24
+*/
+void Sampling::setBoundaries(const Vec<float> &minBoundary, const Vec<float> &maxBoundary) {
+    if (minBoundary.getDim() != maxBoundary.getDim()) {
+        std::cout << "Boudaries have different dimensions" << std::endl;
+        return;
+    }
+    for (unsigned int i = 0; i < minBoundary.getDim(); ++i) {
+        if (minBoundary[i] > maxBoundary[i]) {
+            std::cout << "Min boundary is larger than max boundary" << std::endl;
+            return;
+        }
+    }
+    m_maxBoundary = maxBoundary;
+    m_minBoundary = minBoundary;
+}
+
+/*!
+*  \brief      Check boundary existence
+*  \author     Sascha Kaden
+*  \param[out] result of check
+*  \date       2016-05-24
+*/
+bool Sampling::checkBoudaries() {
+    if (m_maxBoundary.empty() || m_minBoundary.empty()) {
+        std::cout << "Boundaries are empty" << std::endl;
+        return false;
+    }
+    return true;
+}
