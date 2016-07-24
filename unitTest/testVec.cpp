4e1896d6870c54f595286c2aa1b384037c2f8a79
Sascha Kaden <sascha.kaden@s2012.tu-chemnitz.de>
2016-07-24 19:50:54 +0200

    add template and test cases

--- unitTest/testVec.cpp
+++ unitTest/testVec.cpp
@@ -3,28 +3,37 @@
 #define BOOST_TEST_MODULE Vec
 
 #include <boost/test/unit_test.hpp>
+#include <boost/test/test_case_template.hpp>
+#include <boost/mpl/list.hpp>
 
 #include <core/Vec.hpp>
 
 using namespace rmpl;
 
-BOOST_AUTO_TEST_SUITE( constructor )
-BOOST_AUTO_TEST_CASE( constructor1 )
-{
-    Vec<float> obj;
+BOOST_AUTO_TEST_SUITE( constructorFloatDouble )
+typedef boost::mpl::list<float,double> test_types;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE( standardConstructor, T, test_types ) {
+    Vec<T> obj;
     BOOST_CHECK(obj.getDim() == 0);
     BOOST_CHECK_EQUAL(obj.empty(), true);
 }
 
-BOOST_AUTO_TEST_CASE( constructorDimension )
-{
-    unsigned int i = 1;
-    Vec<float> obj(i);
-    BOOST_CHECK(obj.getDim() == i);
-    BOOST_CHECK_EQUAL(obj.empty(), true);
-    //Vec<float> obj((unsigned int) -1);
-    //BOOST_CHECK(obj.getDim() == 0);
-    //BOOST_CHECK_EQUAL(obj.empty(), true);
+BOOST_AUTO_TEST_CASE_TEMPLATE( dimensionContructor, T, test_types ) {
+    for (unsigned int i = 0; i < 10; ++i) {
+        Vec<T> obj(i);
+        BOOST_CHECK(obj.getDim() == i);
+        BOOST_CHECK_EQUAL(obj.empty(), true);
+    }
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE( arrayConstructor, T, test_types ) {
+    T array[10] = {0,1,2,3,4,5,6,7,8,9};
+    Vec<T> obj(10, array);
+    BOOST_CHECK(obj.getDim() == 10);
+    BOOST_CHECK_EQUAL(obj.empty(), false);
+    for (unsigned int i = 0; i < 10; ++i)
+        BOOST_CHECK_EQUAL(obj[i], i);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
