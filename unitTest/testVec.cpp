85fbc4743f193568268d6ec0e614ca4a7568fb17
Sascha Kaden <sascha.kaden@s2012.tu-chemnitz.de>
2016-07-24 20:00:00 +0200

    add test cases

--- unitTest/testVec.cpp
+++ unitTest/testVec.cpp
@@ -11,15 +11,15 @@
 using namespace rmpl;
 
 BOOST_AUTO_TEST_SUITE( constructorFloatDouble )
-typedef boost::mpl::list<float,double> test_types;
+typedef boost::mpl::list<float,double> testTypes;
 
-BOOST_AUTO_TEST_CASE_TEMPLATE( standardConstructor, T, test_types ) {
+BOOST_AUTO_TEST_CASE_TEMPLATE( standardConstructor, T, testTypes ) {
     Vec<T> obj;
     BOOST_CHECK(obj.getDim() == 0);
     BOOST_CHECK_EQUAL(obj.empty(), true);
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE( dimensionContructor, T, test_types ) {
+BOOST_AUTO_TEST_CASE_TEMPLATE( dimensionContructor, T, testTypes ) {
     for (unsigned int i = 0; i < 10; ++i) {
         Vec<T> obj(i);
         BOOST_CHECK(obj.getDim() == i);
@@ -27,13 +27,34 @@ BOOST_AUTO_TEST_CASE_TEMPLATE( dimensionContructor, T, test_types ) {
     }
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE( arrayConstructor, T, test_types ) {
+BOOST_AUTO_TEST_CASE_TEMPLATE( arrayConstructor, T, testTypes ) {
     T array[10] = {0,1,2,3,4,5,6,7,8,9};
     Vec<T> obj(10, array);
     BOOST_CHECK(obj.getDim() == 10);
     BOOST_CHECK_EQUAL(obj.empty(), false);
     for (unsigned int i = 0; i < 10; ++i)
         BOOST_CHECK_EQUAL(obj[i], i);
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE( setAllTo, T, testTypes ) {
+    T array[10] = {0,1,2,3,4,5,6,7,8,9};
+    Vec<T> obj(10, array);
+    obj.setAllTo(0);
+    for (unsigned int i = 0; i < 10; ++i)
+        BOOST_CHECK_EQUAL(obj[i], 0);
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE( append, T, testTypes ) {
+    T array1[10] = {0,1,2,3,4,5,6,7,8,9};
+    T array2[6] = {10,11,12,13,14,15};
+    Vec<T> obj1(10, array1);
+    Vec<T> obj2(6, array2);
+    obj1.append(obj2);
+    for (unsigned int i = 0; i < obj1.getDim(); ++i)
+        BOOST_CHECK_EQUAL(obj1[i], i);
+    obj1.append(Vec<T>()); //append empty Vec
+    for (unsigned int i = 0; i < obj1.getDim(); ++i)
+        BOOST_CHECK_EQUAL(obj1[i], i);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
