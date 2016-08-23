84f7aa9edde9b6452835b0554fb0545255e3341b
Sascha Kaden <sascha.kaden@s2012.tu-chemnitz.de>
2016-08-23 14:23:54 +0200

    add assimp library for reading of cad files

--- include/robot/CadFileLoader.h
+++ include/robot/CadFileLoader.h
@@ -21,9 +21,13 @@
 
 #include <vector>
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 #include <PQP.h>
 
 #include <core/Base.h>
+
 
 namespace rmpl {
 
