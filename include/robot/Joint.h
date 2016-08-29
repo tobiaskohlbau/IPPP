d1602f4ca81c2fc1b101dc2a3e6f855f7ac9f2ca
Sascha Kaden <sascha.kaden@s2012.tu-chemnitz.de>
2016-08-29 15:04:24 +0200

    add Joint class to SerialRobot, it depends the boundaries and the mesh

---
+++ include/robot/Joint.h
@@ -0,0 +1,54 @@
+//-------------------------------------------------------------------------//
+//
+// Copyright 2016 Sascha Kaden
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-------------------------------------------------------------------------//
+
+#ifndef JOINT_H_
+#define JOINT_H_
+
+#include <memory>
+
+#include <fcl/fcl.h>
+#include <PQP.h>
+
+#include <robot/MeshContainer.h>
+
+namespace rmpl {
+
+/*!
+* \brief   Joint class contains the boundaries of a joint and the MeshContainer from it
+* \author  Sascha Kaden
+* \date    2016-08-25
+*/
+class Joint {
+public:
+    Joint(float minBound, float maxBound);
+    Joint(float minBound, float maxBound, std::shared_ptr<MeshContainer> &mesh);
+    void setMesh(std::shared_ptr<MeshContainer> &mesh);
+    std::shared_ptr<MeshContainer> getMesh();
+
+    void setBoundaries(float minBound, float maxBound);
+    void getBoundaries(float &minBound, float &maxBound);
+
+private:
+    float m_minBound;
+    float m_maxBound;
+    std::shared_ptr<MeshContainer> m_mesh;
+};
+
+} /* namespace rmpl */
+
+#endif //JOINT_H_
---
+++ source/robot/Joint.cpp
@@ -0,0 +1,107 @@
+//-------------------------------------------------------------------------//
+//
+// Copyright 2016 Sascha Kaden
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-------------------------------------------------------------------------//
+
+#include <robot/Joint.h>
+
+using namespace rmpl;
+
+/*!
+*  \brief      Standard contructor of the Joint class
+*  \param[in]  minimum Boundary
+*  \param[in]  maximum Boundary
+*  \author     Sascha Kaden
+*  \date       2016-08-25
+*/
+Joint::Joint(float minBound, float maxBound) {
+    if (minBound > maxBound) {
+        m_maxBound = minBound;
+        m_minBound = maxBound;
+    } else {
+        m_minBound = minBound;
+        m_maxBound = maxBound;
+    }
+    m_mesh = nullptr;
+}
+
+/*!
+*  \brief      contructor of the Joint class
+*  \param[in]  minimum Boundary
+*  \param[in]  maximum Boundary
+*  \param[in]  MeshContainer
+*  \author     Sascha Kaden
+*  \date       2016-08-25
+*/
+Joint::Joint(float minBound, float maxBound, std::shared_ptr<MeshContainer> &mesh) {
+    if (minBound > maxBound) {
+        m_maxBound = minBound;
+        m_minBound = maxBound;
+    } else {
+        m_minBound = minBound;
+        m_maxBound = maxBound;
+    }
+    m_mesh = mesh;
+}
+
+/*!
+*  \brief      Set the MeshContainer
+*  \param[in]  MeshContainer
+*  \author     Sascha Kaden
+*  \date       2016-08-25
+*/
+void Joint::setMesh(std::shared_ptr<MeshContainer> &mesh) {
+    m_mesh = mesh;
+}
+
+/*!
+*  \brief      Return the MeshContainer
+*  \param[out] MeshContainer
+*  \author     Sascha Kaden
+*  \date       2016-08-25
+*/
+std::shared_ptr<MeshContainer> Joint::getMesh() {
+    return m_mesh;
+}
+
+/*!
+*  \brief      Set the Boundaries
+*  \param[in]  minimum Boundary
+*  \param[in]  maximum Boundary
+*  \author     Sascha Kaden
+*  \date       2016-08-25
+*/
+void Joint::setBoundaries(float minBound, float maxBound) {
+    if (minBound > maxBound) {
+        m_maxBound = minBound;
+        m_minBound = maxBound;
+    } else {
+        m_minBound = minBound;
+        m_maxBound = maxBound;
+    }
+}
+
+/*!
+*  \brief      Return the Boundaries
+*  \param[out] minimum Boundary
+*  \param[out] maximum Boundary
+*  \author     Sascha Kaden
+*  \date       2016-08-25
+*/
+void Joint::getBoundaries(float &minBound, float &maxBound) {
+    minBound = m_minBound;
+    maxBound = m_maxBound;
+}
