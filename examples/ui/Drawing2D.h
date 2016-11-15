be1cfc1a92247d4c1dd53d2adb15ae2d058b43cf
Sascha Kaden <sascha.kaden@s2012.tu-chemnitz.de>
2016-11-15 18:15:52 +0100

    add drawing of triangle list

--- examples/ui/Drawing2D.h
+++ examples/ui/Drawing2D.h
@@ -6,12 +6,14 @@
 
 #include <core/ModuleBase.h>
 #include <core/Node.h>
+#include <core/Triangle.h>
 
 /*!
 * \brief   Provides methods of drawing form 2D paths with OpenCV
 * \author  Sascha Kaden
 * \date    2016-05-25
 */
+
 class Drawing2D : public rmpl::ModuleBase {
   public:
     static void drawTree2D(const std::vector<std::shared_ptr<rmpl::Node>> &nodes, cv::Mat &image,
@@ -20,6 +22,8 @@ class Drawing2D : public rmpl::ModuleBase {
                             const rmpl::Vec<uint8_t> &colorNode, const rmpl::Vec<uint8_t> &colorEdge, int thickness);
     static void drawPath2D(const std::vector<rmpl::Vec<float>> vecs, cv::Mat &image, const rmpl::Vec<uint8_t> &colorEdge,
                            int thickness);
+    static void drawTrianglePath(const std::vector<rmpl::Vec<float>> vecs, std::vector<rmpl::Triangle> triangles, cv::Mat &image, const rmpl::Vec<uint8_t> &colorEdge,
+                                 int thickness);
 };
 
 #endif /* DRAWING2D_H_ */
