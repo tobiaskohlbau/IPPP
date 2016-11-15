da9bf3786c2c92d31aecb7a65a8d6b7f2abfa6e2
Sascha Kaden <sascha.kaden@s2012.tu-chemnitz.de>
2016-11-15 18:18:45 +0100

    add collision detection for triangle robot and update examples

--- examples/Planner2D/main.cpp
+++ examples/Planner2D/main.cpp
@@ -6,15 +6,96 @@
 #include "opencv2/highgui/highgui.hpp"
 #include <Eigen/Core>
 
-#include <pathPlanner/PRMPlanner.h>
+#include <core/Logging.h>
 #include <pathPlanner/NormalRRTPlanner.h>
+#include <pathPlanner/PRMPlanner.h>
 #include <pathPlanner/StarRRTPlanner.h>
 #include <robot/PointRobot.h>
+#include <robot/TriangleRobot.h>
 
 #include <ui/Drawing2D.h>
+#include <ui/Writer.h>
+
+using namespace rmpl;
+
+cv::Mat obstacleWorkspace;
+
+void testTriangleRobot(Vec<float> min, Vec<float> max, Eigen::MatrixXi mat) {
+    min.append(0);
+    max.append(359);
+    std::shared_ptr<TriangleRobot> triangleRobot(new TriangleRobot(min, max));
+    std::vector<Triangle> triangles;
+    triangles.push_back(Triangle(Vec<float>(0.0, 0.0), Vec<float>(0.0, 25), Vec<float>(25, 0.0)));
+    triangles.push_back(Triangle(Vec<float>(0.0,25), Vec<float>(25,0.0), Vec<float>(50,50)));
+    triangleRobot->setTriangles(triangles);
+    triangleRobot->set2DWorkspace(mat);
+
+    std::shared_ptr<RRTOptions> options(new RRTOptions(30, 0.5, TrajectoryMethod::linear, SamplingMethod::randomly));
+    StarRRTPlanner planner(triangleRobot, options);
+
+    auto startTime = std::chrono::system_clock::now();
+    Vec<float> start(5, 5, 0);
+    Vec<float> goal(400.0, 950.0, 0);
+    bool connected = planner.computePath(start, goal, 35000, 2);
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - startTime);
+    std::cout << "Computation time: " << std::chrono::milliseconds(duration).count() / 1000.0 << std::endl;
+
+    std::vector<std::shared_ptr<Node>> nodes = planner.getGraphNodes();
+    nodes = planner.getPathNodes();
+    if (connected) {
+        Logging::info("Init and goal could be connected!", "Example");
+        std::vector<Vec<float>> path;// = planner.getPath(2, true);
+        for (auto node : nodes)
+            path.push_back(node->getVec());
+        //Writer::writeVecsToFile(path, "result.txt");
+
+        cv::Mat image = obstacleWorkspace.clone();
+        cv::cvtColor(image, image, CV_GRAY2BGR);
+        Drawing2D::drawTrianglePath(path, triangles, image, Vec<uint8_t>(0, 0, 255), 2);
+
+        cv::namedWindow("pathPlanner", CV_WINDOW_AUTOSIZE);
+        cv::imshow("pathPlanner", image);
+        cv::imwrite("result.png", image);
+        cv::waitKey(0);
+    }
+}
+
+void testPointRobot(Vec<float> min, Vec<float> max, Eigen::MatrixXi mat) {
+    std::shared_ptr<PointRobot> robot(new PointRobot(min, max));
+    robot->set2DWorkspace(mat);
+    std::shared_ptr<RRTOptions> options(new RRTOptions(50, 0.5, TrajectoryMethod::linear, SamplingMethod::randomly));
+    StarRRTPlanner planner(robot, options);
+    NormalRRTPlanner planner1(robot, options);
+    // compute the tree
+    auto startTime = std::chrono::system_clock::now();
+    Vec<float> start(50.0, 30.0);
+    Vec<float> goal(870.0, 870.0);
+    // planner.setInitNode(start);
+    // planner.computeTree(4000, 2);
+    // bool connected = planner.connectGoalNode(goal);
+    bool connected = planner.computePath(start, goal, 4000, 2);
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - startTime);
+    std::cout << "Computation time: " << std::chrono::milliseconds(duration).count() / 1000.0 << std::endl;
+    std::vector<std::shared_ptr<Node>> nodes = planner.getGraphNodes();
+
+    cv::Mat image = obstacleWorkspace.clone();
+    cv::cvtColor(image, image, CV_GRAY2BGR);
+
+    Drawing2D::drawTree2D(nodes, image, Vec<uint8_t>(0, 0, 255), Vec<uint8_t>(0, 0, 0), 1);
+
+    if (connected) {
+        std::vector<Vec<float>> pathPoints = planner.getPath(0.5, true);
+        Drawing2D::drawPath2D(pathPoints, image, Vec<uint8_t>(255, 0, 0), 3);
+    }
+
+    cv::namedWindow("pathPlanner", CV_WINDOW_AUTOSIZE);
+    cv::imshow("pathPlanner", image);
+    cv::imwrite("result.png", image);
+    cv::waitKey(0);
+}
 
 int main(int argc, char** argv) {
-    cv::Mat freeWorkspace, obstacleWorkspace;
+
     // obstacleWorkspace = cv::imread("spaces/freeWorkspace.png", CV_LOAD_IMAGE_GRAYSCALE);
     // obstacleWorkspace = cv::imread("spaces/labyrinth.png", CV_LOAD_IMAGE_GRAYSCALE);
     obstacleWorkspace = cv::imread("spaces/obstacleWorkspace.png", CV_LOAD_IMAGE_GRAYSCALE);
@@ -35,38 +116,10 @@ int main(int argc, char** argv) {
     }
     mat = Eigen::MatrixXi::Map(&entries[0], rows, cols);
 
-    rmpl::Vec<float> minBoundary(0.0, 0.0);
-    rmpl::Vec<float> maxBoundary(rows, cols);
-    std::shared_ptr<rmpl::PointRobot> robot(new rmpl::PointRobot(minBoundary, maxBoundary));
-    robot->set2DWorkspace(mat);
-    std::shared_ptr<rmpl::RRTOptions> options(
-        new rmpl::RRTOptions(50, 0.5, rmpl::TrajectoryMethod::linear, rmpl::SamplingMethod::randomly));
-    rmpl::StarRRTPlanner planner(robot, options);
-    rmpl::NormalRRTPlanner planner1(robot, options);
-    // compute the tree
-    auto startTime = std::chrono::system_clock::now();
-    rmpl::Vec<float> start(50.0, 30.0);
-    rmpl::Vec<float> goal(870.0, 870.0);
-    // planner.setInitNode(start);
-    // planner.computeTree(4000, 2);
-    // bool connected = planner.connectGoalNode(goal);
-    bool connected = planner.computePath(start, goal, 4000, 2);
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - startTime);
-    std::cout << "Computation time: " << std::chrono::milliseconds(duration).count() / 1000.0 << std::endl;
-    std::vector<std::shared_ptr<rmpl::Node>> nodes = planner.getGraphNodes();
+    Vec<float> minBoundary(0.0, 0.0);
+    Vec<float> maxBoundary(rows, cols);
 
-    cv::Mat image = obstacleWorkspace.clone();
-    cv::cvtColor(image, image, CV_GRAY2BGR);
+    testTriangleRobot(minBoundary, maxBoundary, mat);
 
-    Drawing2D::drawTree2D(nodes, image, rmpl::Vec<uint8_t>(0, 0, 255), rmpl::Vec<uint8_t>(0, 0, 0), 1);
-
-    if (connected) {
-        std::vector<rmpl::Vec<float>> pathPoints = planner.getPath(0.5, true);
-        Drawing2D::drawPath2D(pathPoints, image, rmpl::Vec<uint8_t>(255, 0, 0), 3);
-    }
-
-    cv::namedWindow("pathPlanner", CV_WINDOW_AUTOSIZE);
-    cv::imshow("pathPlanner", image);
-    cv::imwrite("result.png", image);
-    cv::waitKey(0);
+    //testPointRobot(minBoundary, maxBoundary, mat);
 }
--- include/core/CollisionDetection.h
+++ include/core/CollisionDetection.h
@@ -26,10 +26,11 @@
 
 #include <core/ModuleBase.h>
 #include <core/Node.h>
+#include <core/Triangle.h>
 #include <robot/MeshContainer.h>
 #include <robot/MobileRobot.h>
-#include <robot/RobotBase.h>
 #include <robot/SerialRobot.h>
+#include <robot/TriangleRobot.h>
 
 namespace rmpl {
 
@@ -50,19 +51,21 @@ class CollisionDetection : public ModuleBase {
     bool checkMobileRobot(const Vec<float> &vec);
     bool checkMesh(std::vector<std::shared_ptr<PQP_Model>> &models, std::shared_ptr<PQP_Model> &base, Eigen::Matrix3f R[],
                    Eigen::Matrix3f &poseR, Eigen::Vector3f t[], Eigen::Vector3f &poseT);
-    bool checkMesh(std::vector<std::shared_ptr<fcl::BVHModel<fcl::OBBRSS<float>>>> &models,
-                   std::shared_ptr<fcl::BVHModel<fcl::OBBRSS<float>>> & base, Eigen::Matrix3f R[], Eigen::Matrix3f &poseR,
+    bool checkMesh(std::vector<std::shared_ptr<FCLModel>> &models,
+                   std::shared_ptr<FCLModel> & base, Eigen::Matrix3f R[], Eigen::Matrix3f &poseR,
                    Eigen::Vector3f t[], Eigen::Vector3f &poseT);
 
     bool checkPQP(std::shared_ptr<PQP_Model> &model1, std::shared_ptr<PQP_Model> &model2, Eigen::Matrix3f &R1,
                   Eigen::Matrix3f &R2, Eigen::Vector3f &t1, Eigen::Vector3f &t2);
-    bool checkFCL(std::shared_ptr<fcl::BVHModel<fcl::OBBRSS<float>>> &model1,
-                  std::shared_ptr<fcl::BVHModel<fcl::OBBRSS<float>>> &model2, Eigen::Matrix3f &R1, Eigen::Matrix3f &R2,
+    bool checkFCL(std::shared_ptr<FCLModel> &model1,
+                  std::shared_ptr<FCLModel> &model2, Eigen::Matrix3f &R1, Eigen::Matrix3f &R2,
                   Eigen::Vector3f &t1, Eigen::Vector3f &t2);
 
-    bool checkPointRobot(float x, float y);
+    bool checkPoint2D(float x, float y);
+    bool checkTriangleRobot(const Vec<float> &vec);
 
     std::shared_ptr<RobotBase> m_robot;
+    Vec<float> m_minBoundary, m_maxBoundary;
 
     Eigen::MatrixXi m_2DWorkspace;
     std::shared_ptr<MeshContainer> m_workspace = nullptr;
