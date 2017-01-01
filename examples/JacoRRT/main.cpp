093d78bae2f12bbf1a022d7439a76dc22909562f
Sascha Kaden <sascha.kaden@s2012.tu-chemnitz.de>
2017-01-01 15:09:23 +0100

    update Jaco models, from obj to STL

--- examples/JacoRRT/main.cpp
+++ examples/JacoRRT/main.cpp
@@ -21,10 +21,14 @@ void printTime(clock_t begin, clock_t end) {
 
 void simpleRRT() {
     std::shared_ptr<Jaco> robot(new Jaco());
+
+    robot->saveMeshConfig(utilVec::Vecf(0, 0, 0, 0, 0, 0));
+    return;
+
     RRTOptions options(30, 0.5);
     NormalRRTPlanner<6> planner(robot, options);
-    Eigen::Matrix<float, 6, 1> start = utilVec::Vecf(180, 180, 180, 180, 180, 180);
-    Eigen::Matrix<float, 6, 1> goal = utilVec::Vecf(275, 167.5, 57.4, 241, 82.7, 75.5);
+    Vector6 start = utilVec::Vecf(180, 180, 180, 180, 180, 180);
+    Vector6 goal = utilVec::Vecf(275, 167.5, 57.4, 241, 82.7, 75.5);
 
     // compute the tree
     clock_t begin = std::clock();
@@ -33,7 +37,7 @@ void simpleRRT() {
     printTime(begin, end);
 
     std::vector<std::shared_ptr<Node<6>>> nodes = planner.getGraphNodes();
-    std::vector<Eigen::Matrix<float, 6 ,1>> graphPoints;
+    std::vector<Vector6> graphPoints;
     std::cout << "Init Graph has: " << nodes.size() << "nodes" << std::endl;
     for (int i = 0; i < nodes.size(); ++i)
         graphPoints.push_back(robot->directKinematic(nodes[i]->getValues()));
@@ -41,9 +45,9 @@ void simpleRRT() {
 
     if (connected) {
         std::cout << "Init and goal could be connected!" << std::endl;
-        std::vector<Eigen::Matrix<float, 6, 1>> pathAngles = planner.getPath(5, true);
+        std::vector<Vector6> pathAngles = planner.getPath(5, true);
 
-        std::vector<Eigen::Matrix<float, 6, 1>> pathPoints;
+        std::vector<Vector6> pathPoints;
         for (auto angles : pathAngles)
             pathPoints.push_back(robot->directKinematic(angles));
         writer::appendVecsToFile<6>(pathPoints, "example.ASC", 10);
@@ -93,7 +97,7 @@ void treeConnection() {
     }
 
     std::vector<std::shared_ptr<Node<6>>> nodes = plannerInitNode.getGraphNodes();
-    std::vector<Eigen::Matrix<float, 6, 1>> graphPoints;
+    std::vector<Vector6> graphPoints;
     std::cout << "Init Graph has: " << nodes.size() << "nodes" << std::endl;
     for (int i = 0; i < nodes.size(); ++i)
         graphPoints.push_back(robot->directKinematic(nodes[i]->getValues()));
