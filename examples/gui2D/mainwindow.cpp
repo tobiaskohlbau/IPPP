77825650dbc463167bd89fe0dcceb2a18fe1cd0a
Sascha Kaden <kaden.sascha@googlemail.com>
2017-02-09 18:55:41 +0100

    update Gui2D

--- examples/gui2D/mainwindow.cpp
+++ examples/gui2D/mainwindow.cpp
@@ -1,8 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-#include <iostream>
-
 #include <QFileDialog>
 #include <core/types.h>
 #include <pathPlanner/options/PRMOptions.h>
@@ -12,6 +10,14 @@ using namespace rmpl;
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    m_widget = new QWidget();
+    m_widget->setWindowTitle(QString("Path"));
+    m_layout = new QVBoxLayout();
+    m_widget->setLayout(m_layout);
+
+    m_triangles.push_back(Triangle2D(Vector2(0, 0), Vector2(25, 0 ), Vector2(25, 50)));
+    m_triangles.push_back(Triangle2D(Vector2(0, 0), Vector2(0 , 50), Vector2(25, 50)));
 }
 
 MainWindow::~MainWindow() {
@@ -36,24 +42,19 @@ void MainWindow::loadImage() {
 
 void MainWindow::computePath() {
     if (!m_imageLoaded) {
-        Logging::error("image was not loaded", "GUI");
+        Logging::error("Image was not loaded", "GUI");
         return;
     }
-    Logging::info("compute path", "GUI");
-    std::cout << m_numNodes << std::endl;
-    Vector2 minBoundary(0.0, 0.0);
-    Vector2 maxBoundary(m_workspace.rows(), m_workspace.cols());
-    std::shared_ptr<PointRobot> robot(new PointRobot(minBoundary, maxBoundary));
-    robot->set2DWorkspace(m_workspace);
+    Logging::info("Compute path ...", "GUI");
 
     SamplerMethod sampler = SamplerMethod::randomly;
-    if (m_samplingType == 1)
+    if (m_samplingStrategy == 1)
         sampler = SamplerMethod::uniform;
-    else if (m_samplerType == 2)
+    else if (m_samplerMethod == 2)
         sampler = SamplerMethod::standardDistribution;
 
     SamplingStrategy sampling = SamplingStrategy::normal;
-    if (m_samplingType == 1)
+    if (m_samplingStrategy == 1)
         sampling = SamplingStrategy::nearObstacles;
 
     rmpl::EdgeHeuristic edgeH = rmpl::EdgeHeuristic::L2;
@@ -68,22 +69,41 @@ void MainWindow::computePath() {
     else if (m_edgeHeuristic == 5)
         edgeH = rmpl::EdgeHeuristic::WeightVec_INF;
 
-    m_planner = nullptr;
-    if (m_plannerType == 0) {
-        RRTOptions options(m_rrtStepsize, m_trajectoryStepSize, sampler, sampling, edgeH);
-        m_planner = std::shared_ptr<NormalRRTPlanner<2>>(new NormalRRTPlanner<2>(robot, options));
-    } else if (m_plannerType == 1) {
-        RRTOptions options(m_rrtStepsize, m_trajectoryStepSize, sampler, sampling, edgeH);
-        m_planner = std::shared_ptr<StarRRTPlanner<2>>(new StarRRTPlanner<2>(robot, options));
+    RRTOptions rrtOptions(m_rrtStepsize, m_trajectoryStepSize, sampler, sampling, edgeH);
+    PRMOptions prmOptions(m_prmDistance, m_trajectoryStepSize, sampler, sampling, edgeH);
+
+    if (m_robotType == 1) {
+        Vector3 minBoundary(0.0, 0.0, 0.0);
+        Vector3 maxBoundary(m_workspace.rows(), m_workspace.cols(), 360);
+        std::shared_ptr<TriangleRobot2D> robot(new TriangleRobot2D(m_triangles, minBoundary, maxBoundary));
+        robot->set2DWorkspace(m_workspace);
+
+        if (m_plannerType == 0)
+            m_planner3d = std::shared_ptr<NormalRRTPlanner<3>>(new NormalRRTPlanner<3>(robot, rrtOptions));
+        else if (m_plannerType == 1)
+            m_planner3d = std::shared_ptr<StarRRTPlanner<3>>(new StarRRTPlanner<3>(robot, rrtOptions));
+        else
+            m_planner3d = std::shared_ptr<PRMPlanner<3>>(new PRMPlanner<3>(robot, prmOptions));
+        Vector3 start(m_startX, m_startY, m_startPhi);
+        Vector3 goal(m_goalX, m_goalY, m_goalPhi);
+        m_connected = m_planner3d->computePath(start, goal, m_numNodes, m_numThreads);
     } else {
-        PRMOptions options(m_prmDistance, m_trajectoryStepSize, sampler, sampling, edgeH);
-        m_planner = std::shared_ptr<PRMPlanner<2>>(new PRMPlanner<2>(robot, options));
+        Vector2 minBoundary(0.0, 0.0);
+        Vector2 maxBoundary(m_workspace.rows(), m_workspace.cols());
+        std::shared_ptr<PointRobot> robot(new PointRobot(minBoundary, maxBoundary));
+        robot->set2DWorkspace(m_workspace);
+
+        if (m_plannerType == 0)
+            m_planner2d = std::shared_ptr<NormalRRTPlanner<2>>(new NormalRRTPlanner<2>(robot, rrtOptions));
+        else if (m_plannerType == 1)
+            m_planner2d = std::shared_ptr<StarRRTPlanner<2>>(new StarRRTPlanner<2>(robot, rrtOptions));
+        else
+            m_planner2d = std::shared_ptr<PRMPlanner<2>>(new PRMPlanner<2>(robot, prmOptions));
+        Vector2 start(m_startX, m_startY);
+        Vector2 goal(m_goalX, m_goalY);
+        m_connected = m_planner2d->computePath(start, goal, m_numNodes, m_numThreads);
     }
-
-    Vector2 start(m_startX, m_startY);
-    Vector2 goal(m_goalX, m_goalY);
-    m_connected = m_planner->computePath(start, goal, m_numNodes, m_numThreads);
-
+    Logging::info("... done", "GUI");
     viewPath();
 }
 
@@ -91,21 +111,36 @@ void MainWindow::viewPath() {
     cv::Mat image = m_image.clone();
     cv::cvtColor(image, image, CV_GRAY2BGR);
 
-    std::vector<std::shared_ptr<Node<2>>> nodes = m_planner->getGraphNodes();
-    drawing::drawTree2D<2>(nodes, image, Eigen::Vector3i(0, 0, 255), Eigen::Vector3i(0, 0, 0), 1);
-    if (m_connected) {
-        std::vector<Vector2> pathPoints = m_planner->getPath(1, true);
-        drawing::drawPath2D(pathPoints, image, Eigen::Vector3i(255, 0, 0), 3);
+    if (m_robotType == 1) {
+        if (m_connected) {
+            std::vector<Vector3> path = m_planner3d->getPath(80, true);
+            drawing::drawTrianglePath(path, m_triangles, image, Eigen::Vector3i(0, 0, 255), 2);
+        }
+        else {
+            return;
+        }
+    } else {
+        std::vector<std::shared_ptr<Node<2>>> nodes = m_planner2d->getGraphNodes();
+        drawing::drawTree2D<2>(nodes, image, Eigen::Vector3i(0, 0, 255), Eigen::Vector3i(0, 0, 0), 1);
+        if (m_connected) {
+            std::vector<Vector2> pathPoints = m_planner2d->getPath(1, true);
+            drawing::drawPath2D(pathPoints, image, Eigen::Vector3i(255, 0, 0), 3);
+        }
     }
 
     QImage qImage = convertCvMat(image);
-
     QPixmap qPixmap = QPixmap::fromImage(qImage);
-    scene = new QGraphicsScene(this);
+    QGraphicsScene* scene = new QGraphicsScene();
     scene->addPixmap(qPixmap);
     scene->setSceneRect(qPixmap.rect());
 
-    ui->mainImage->setScene(scene);
+    QLayoutItem* item = m_widget->layout()->takeAt( 0 );
+    if (item != NULL)
+        delete item->widget();
+
+    QGraphicsView * view = new QGraphicsView(scene);
+    m_layout->addWidget(view);
+    m_widget->show();
 }
 
 QImage MainWindow::convertCvMat(cv::Mat inMat) {
@@ -139,51 +174,130 @@ QImage MainWindow::convertCvMat(cv::Mat inMat) {
     return QImage();
 }
 
-void MainWindow::Threads(int num) {
+void MainWindow::loadConfig() {
+    QString qFileName = QFileDialog::getOpenFileName(this, tr("Open Config"), "/home", tr("Config Files (*.js *.json)"));
+    boost::property_tree::ptree pt;
+    boost::property_tree::read_json(qFileName.toStdString(), pt);
+
+    updateThreads(pt.get<int>("numThreads"));
+    updateNodes(pt.get<int>("numNodes"));
+    updatePlannerType(pt.get<int>("plannerType"));
+    updateRobotType(pt.get<int>("robotType"));
+    updateStartX(pt.get<int>("startX"));
+    updateStartY(pt.get<int>("startY"));
+    updateStartPhi(pt.get<double>("startPhi"));
+    updateGoalX(pt.get<int>("goalX"));
+    updateGoalY(pt.get<int>("goalY"));
+    updateGoalPhi(pt.get<double>("goalPhi"));
+    updateTrajectoryStepSize(pt.get<double>("trajectoryStepSize"));
+    updateSamplerType(pt.get<int>("samplerMethod"));
+    updateSamplingType(pt.get<int>("samplingStrategy"));
+    updateEdgeHeuristic(pt.get<int>("edgeHeuristic"));
+    updatePRMDistance(pt.get<double>("prmDistance"));
+    updateRRTStepSize(pt.get<double>("rrtStepSize"));
+    updateWeightVecX(pt.get<double>("weightVecX"));
+    updateWeightVecY(pt.get<double>("weightVecY"));
+
+    setNumThreads(m_numThreads);
+    setNumNodes(m_numNodes);
+    setPlannerType(m_plannerType);
+    setRobotType(m_robotType);
+    setTrajectoryStepSize(m_trajectoryStepSize);
+    setSamplerMethod(m_samplerMethod);
+    setSamplingStrategy(m_samplingStrategy);
+    setPrmDistance(m_prmDistance);
+    setRrtStepSize(m_rrtStepsize);
+    setStartX(m_startX);
+    setStartY(m_startY);
+    setStartPhi(m_startPhi);
+    setGoalX(m_goalX);
+    setGoalY(m_goalY);
+    setGoalPhi(m_goalPhi);
+    setEdgeHeuristic(m_edgeHeuristic);
+    setWeightVecX(m_weightVecX);
+    setWeightVecY(m_weightVecY);
+}
+
+void MainWindow::saveConfig() {
+    boost::property_tree::ptree pt;
+
+    pt.put("numThreads", m_numThreads);
+    pt.put("numNodes", m_numNodes);
+    pt.put("plannerType", m_plannerType);
+    pt.put("robotType", m_robotType);
+    pt.put("trajectoryStepSize", m_trajectoryStepSize);
+    pt.put("samplerMethod", m_samplerMethod);
+    pt.put("samplingStrategy", m_samplingStrategy);
+    pt.put("prmDistance", m_prmDistance);
+    pt.put("rrtStepSize", m_rrtStepsize);
+    pt.put("startX", m_startX);
+    pt.put("startY", m_startY);
+    pt.put("startPhi", m_startPhi);
+    pt.put("goalX", m_goalX);
+    pt.put("goalY", m_goalY);
+    pt.put("goalPhi", m_goalPhi);
+    pt.put("edgeHeuristic", m_edgeHeuristic);
+    pt.put("weightVecX", m_weightVecX);
+    pt.put("weightVecY", m_weightVecY);
+
+    QString qFileName = QFileDialog::getSaveFileName(this, tr("Save Config"), "/home", tr("Config Files (*.js *.json)"));
+    boost::property_tree::write_json(qFileName.toStdString(), pt);
+}
+
+void MainWindow::updateThreads(int num) {
     m_numThreads = num;
-};
-void MainWindow::Nodes(int num) {
+}
+void MainWindow::updateNodes(int num) {
     m_numNodes = num;
-};
-void MainWindow::PlannerType(int type) {
+}
+void MainWindow::updatePlannerType(int type) {
     m_plannerType = type;
-};
-void MainWindow::StartX(int value) {
-    m_startX = value;
-};
-void MainWindow::StartY(int value) {
-    m_startY = value;
-};
-void MainWindow::GoalX(int value) {
-    m_goalX = value;
-};
-void MainWindow::GoalY(int value) {
-    m_goalX = value;
-};
-void MainWindow::TrajectoryStepSize(double value) {
+}
+void MainWindow::updateRobotType(int type) {
+    m_robotType = type;
+}
+void MainWindow::updateTrajectoryStepSize(double value) {
     m_trajectoryStepSize = value;
-};
-void MainWindow::SamplingType(int type) {
-    m_samplingType = type;
-};
-void MainWindow::SamplerType(int type) {
-    m_samplerType = type;
-};
-void MainWindow::EdgeHeuristic(int type) {
-    m_edgeHeuristic = type;
-};
-void MainWindow::PRMDistance(double value) {
+}
+void MainWindow::updateSamplingType(int type) {
+    m_samplingStrategy = type;
+}
+void MainWindow::updateSamplerType(int type) {
+    m_samplerMethod = type;
+}
+void MainWindow::updatePRMDistance(double value) {
     m_prmDistance = value;
-};
-void MainWindow::RRTStepSize(double value) {
+}
+void MainWindow::updateRRTStepSize(double value) {
     m_rrtStepsize = value;
-};
-void MainWindow::weightVecX(double value) {
+}
+void MainWindow::updateStartX(int value) {
+    m_startX = value;
+}
+void MainWindow::updateStartY(int value) {
+    m_startY = value;
+}
+void MainWindow::updateStartPhi(double value) {
+    m_startPhi = value;
+}
+void MainWindow::updateGoalX(int value) {
+    m_goalX = value;
+}
+void MainWindow::updateGoalY(int value) {
+    m_goalY = value;
+}
+void MainWindow::updateGoalPhi(double value) {
+    m_goalPhi = value;
+}
+void MainWindow::updateEdgeHeuristic(int type) {
+    m_edgeHeuristic = type;
+}
+void MainWindow::updateWeightVecX(double value) {
     m_weightVecX = value;
     Vector2 weight(m_weightVecX, m_weightVecY);
     Heuristic<2>::setWeightVec(weight);
 }
-void MainWindow::weightVecY(double value) {
+void MainWindow::updateWeightVecY(double value) {
     m_weightVecY = value;
     Vector2 weight(m_weightVecX, m_weightVecY);
     Heuristic<2>::setWeightVec(weight);
--- examples/gui2D/mainwindow.h
+++ examples/gui2D/mainwindow.h
@@ -1,8 +1,13 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <QVBoxLayout>
 #include <QGraphicsScene>
+#include <QGraphicsView>
 #include <QMainWindow>
+#include <QLabel>
 
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
@@ -13,6 +18,7 @@
 #include <pathPlanner/PRMPlanner.hpp>
 #include <pathPlanner/StarRRTPlanner.hpp>
 #include <robot/PointRobot.h>
+#include <robot/TriangleRobot2D.h>
 
 namespace Ui {
 class MainWindow;
@@ -26,50 +32,82 @@ class MainWindow : public QMainWindow {
     ~MainWindow();
 
   public slots:
+    void loadConfig();
+    void saveConfig();
     void loadImage();
     void computePath();
-    void Threads(int num);
-    void Nodes(int num);
-    void PlannerType(int type);
-    void StartX(int value);
-    void StartY(int value);
-    void GoalX(int value);
-    void GoalY(int value);
-    void TrajectoryStepSize(double value);
-    void SamplerType(int type);
-    void SamplingType(int type);
-    void EdgeHeuristic(int type);
-    void PRMDistance(double value);
-    void RRTStepSize(double value);
-    void weightVecX(double value);
-    void weightVecY(double value);
+    void updateThreads(int num);
+    void updateNodes(int num);
+    void updatePlannerType(int type);
+    void updateRobotType(int type);
+    void updateStartX(int value);
+    void updateStartY(int value);
+    void updateStartPhi(double value);
+    void updateGoalX(int value);
+    void updateGoalY(int value);
+    void updateGoalPhi(double value);
+    void updateTrajectoryStepSize(double value);
+    void updateSamplerType(int type);
+    void updateSamplingType(int type);
+    void updateEdgeHeuristic(int type);
+    void updatePRMDistance(double value);
+    void updateRRTStepSize(double value);
+    void updateWeightVecX(double value);
+    void updateWeightVecY(double value);
+  signals:
+    void setNumThreads(int value);
+    void setNumNodes(int value);
+    void setPlannerType(int type);
+    void setRobotType(int type);
+    void setTrajectoryStepSize(double value);
+    void setSamplerMethod(int type);
+    void setSamplingStrategy(int type);
+    void setPrmDistance(double value);
+    void setRrtStepSize(double value);
+    void setStartX(int value);
+    void setStartY(int value);
+    void setStartPhi(double value);
+    void setGoalX(int value);
+    void setGoalY(int value);
+    void setGoalPhi(double phi);
+    void setEdgeHeuristic(int type);
+    void setWeightVecX(double value);
+    void setWeightVecY(double value);
 
   private:
     void viewPath();
     QImage convertCvMat(cv::Mat image);
 
     Ui::MainWindow *ui;
-    QGraphicsScene *scene;
+    QWidget *m_widget;
+    QLayout * m_layout;
 
     unsigned int m_numThreads = 2;
     unsigned int m_numNodes = 5000;
     unsigned int m_plannerType = 0;
+    unsigned int m_robotType = 0;
     int m_startX = 30;
     int m_startY = 30;
+    double m_startPhi = 0;
     int m_goalX = 870;
     int m_goalY = 870;
+    double m_goalPhi = 0;
     double m_trajectoryStepSize = 1;
-    unsigned int m_samplerType = 0;
-    unsigned int m_samplingType = 0;
+    unsigned int m_samplerMethod = 0;
+    unsigned int m_samplingStrategy = 0;
     unsigned int m_edgeHeuristic = 0;
     double m_prmDistance = 50;
     double m_rrtStepsize = 50;
     double m_weightVecX = 1;
     double m_weightVecY = 1;
+    QString m_configPath;
 
-    std::shared_ptr<rmpl::Planner<2>> m_planner;
     cv::Mat m_image;
     Eigen::MatrixXi m_workspace;
+
+    std::vector<rmpl::Triangle2D> m_triangles;
+    std::shared_ptr<rmpl::Planner<2>> m_planner2d = nullptr;
+    std::shared_ptr<rmpl::Planner<3>> m_planner3d = nullptr;
     bool m_connected = false;
     bool m_imageLoaded = false;
 };
