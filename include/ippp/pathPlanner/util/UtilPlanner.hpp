597525ca536acf953c37857b17f19ae20b1a15ad
Sascha Kaden <kaden.sascha@googlemail.com>
2017-10-03 13:21:28 +0200

    move A* algorithm from PRM to UtilPlanner

--- include/ippp/pathPlanner/util/UtilPlanner.hpp
+++ include/ippp/pathPlanner/util/UtilPlanner.hpp
@@ -35,8 +35,8 @@ namespace util {
 */
 template <unsigned int dim>
 static std::shared_ptr<Node<dim>> getNearestValidNode(const Vector<dim> &config, const std::shared_ptr<Graph<dim>> &graph,
-                                                      const std::shared_ptr<TrajectoryPlanner<dim>> &trajectory,
-                                                      const std::shared_ptr<DistanceMetric<dim>> &metric, const double range) {
+                                               const std::shared_ptr<TrajectoryPlanner<dim>> &trajectory,
+                                               const std::shared_ptr<DistanceMetric<dim>> &metric, const double range) {
     std::shared_ptr<Node<dim>> nearestNode = nullptr;
     std::vector<std::shared_ptr<Node<dim>>> nearNodes = graph->getNearNodes(config, range);
     double dist = std::numeric_limits<double>::max();
@@ -48,6 +48,75 @@ static std::shared_ptr<Node<dim>> getNearestValidNode(const Vector<dim> &config,
         }
     }
     return nearestNode;
+}
+
+/*!
+*  \brief         Expands the openList of the A* algorithm from the childes of the passed Node
+*  \author        Sascha Kaden
+*  \param[in]     current node
+*  \param[in,out] open list
+*  \param[in,out] closed list
+*  \param[in]     DistanceMetric
+*  \date          2016-08-09
+*/
+template <unsigned int dim>
+static void expandNode(const std::shared_ptr<Node<dim>> currentNode, std::vector<std::shared_ptr<Node<dim>>> &openList,
+                std::vector<std::shared_ptr<Node<dim>>> &closedList, const std::shared_ptr<DistanceMetric<dim>> &metric) {
+    double dist, edgeCost;
+    for (auto successor : currentNode->getChildNodes()) {
+        if (util::contains(closedList, successor))
+            continue;
+
+        edgeCost = metric->calcDist(currentNode, successor);
+        dist = currentNode->getCost() + edgeCost;
+
+        if (util::contains(openList, successor) && dist >= successor->getCost())
+            continue;
+
+        successor->setQueryParent(currentNode, edgeCost);
+        successor->setCost(dist);
+        if (!util::contains(openList, successor))
+            openList.push_back(successor);
+    }
+}
+
+/*!
+*  \brief      A* algorithm to find best path
+*  \details    Query parents of all nodes has to be cleared at the Graph.
+*  \author     Sascha Kaden
+*  \param[in]  source Node (start)
+*  \param[in]  goal Node (goal)
+*  \param[in]  DistanceMetric
+*  \param[out] result of algorithm
+*  \date       2016-08-09
+*/
+template <unsigned int dim>
+static bool aStar(const std::shared_ptr<Node<dim>> sourceNode, const std::shared_ptr<Node<dim>> targetNode,
+           const std::shared_ptr<DistanceMetric<dim>> &metric) {
+    std::vector<std::shared_ptr<Node<dim>>> openList, closedList;
+
+    std::vector<std::shared_ptr<Edge<dim>>> edges = sourceNode->getChildEdges();
+    for (int i = 0; i < edges.size(); ++i) {
+        edges[i]->getTarget()->setCost(edges[i]->getCost());
+        edges[i]->getTarget()->setQueryParent(sourceNode, metric->calcDist(edges[i]->getTarget(), sourceNode));
+        openList.push_back(edges[i]->getTarget());
+    }
+    closedList.push_back(sourceNode);
+
+    int count = 0;
+    std::shared_ptr<Node<dim>> currentNode;
+    while (!openList.empty()) {
+        currentNode = util::removeMinFromList(openList);
+
+        if (currentNode == targetNode) {
+            return true;
+        }
+        closedList.push_back(currentNode);
+        ++count;
+
+        expandNode<dim>(currentNode, openList, closedList, metric);
+    }
+    return false;
 }
 
 } /* namespace util */
