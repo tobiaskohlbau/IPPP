d914641c871e1f2801c52b9da7e4ad2c767b9874
Sascha Kaden <kaden.sascha@googlemail.com>
2017-10-16 12:50:49 +0200

    Create Configurator to save and load json files

---
+++ examples/ui/Configurator.cpp
@@ -0,0 +1,77 @@
+//-------------------------------------------------------------------------//
+//
+// Copyright 2017 Sascha Kaden
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-------------------------------------------------------------------------//
+
+#include <ui/Configurator.h>
+
+#include <fstream>
+
+#include <ippp/core/util/Logging.h>
+
+namespace ippp {
+
+/*!
+*  \brief      Standard constructor of the Configurator
+*  \author     Sascha Kaden
+*  \param[in]  name
+*  \date       2017-10-16
+*/
+Configurator::Configurator(const std::string &name) : Identifier(name) {
+}
+
+/*!
+*  \brief      Load a json file from the passed file path.
+*  \author     Sascha Kaden
+*  \param[in]  file path
+*  \param[out] json
+*  \date       2017-10-16
+*/
+nlohmann::json Configurator::loadJson(const std::string &filePath) {
+    if (filePath.empty()) {
+        Logging::error("Empty file path!", this);
+        return nlohmann::json();
+    }
+    std::ifstream i(filePath);
+    nlohmann::json j;
+    i >> j;
+    return j;
+}
+
+/*!
+*  \brief      Saves the json data to the passed file path.
+*  \author     Sascha Kaden
+*  \param[in]  file path
+*  \param[in]  json data
+*  \param[out] validation of the saving
+*  \date       2017-10-16
+*/
+bool Configurator::saveJson(const std::string &filePath, const nlohmann::json &data) {
+    if (filePath.empty()) {
+        Logging::error("Empty file path!", this);
+        return false;
+    }
+    if (data.empty()) {
+        Logging::warning("Empty data!", this);
+        return false;
+    }
+
+    std::ofstream o(filePath);
+    o << std::setw(4) << data << std::endl;
+    return true;
+}
+
+} /* namespace ippp */
---
+++ examples/ui/Configurator.h
@@ -0,0 +1,84 @@
+//-------------------------------------------------------------------------//
+//
+// Copyright 2017 Sascha Kaden
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-------------------------------------------------------------------------//
+
+#ifndef CONFIGURATOR_H
+#define CONFIGURATOR_H
+
+#include <string>
+
+#include <json.hpp>
+
+#include <ippp/core/Identifier.h>
+#include <ippp/core/types.h>
+
+namespace ippp {
+
+/*!
+* \brief   Class Configurator is the base class to load and save json files.
+* \author  Sascha Kaden
+* \date    2017-10-16
+*/
+class Configurator : public Identifier {
+  public:
+    Configurator(const std::string &name);
+
+  protected:
+    nlohmann::json loadJson(const std::string &filePath);
+    bool saveJson(const std::string &filePath, const nlohmann::json &data);
+
+    template <unsigned int dim>
+    std::string vectorToString(const Vector<dim> &vector);
+    template <unsigned int dim>
+    Vector<dim> stringToVector(const std::string &string);
+};
+
+/*!
+*  \brief      Converts a Vector to a string
+*  \author     Sascha Kaden
+*  \param[in]  Vector
+*  \param[out] string
+*  \date       2017-10-16
+*/
+template <unsigned int dim>
+std::string Configurator::vectorToString(const Vector<dim> &vector) {
+    std::string string;
+    for (unsigned int i = 0; i < dim; ++i)
+        string += std::to_string(vector[i]) + " ";
+    return string;
+}
+
+/*!
+*  \brief      Converts a string to a Vector
+*  \author     Sascha Kaden
+*  \param[in]  string
+*  \param[out] Vector
+*  \date       2017-10-16
+*/
+template <unsigned int dim>
+Vector<dim> Configurator::stringToVector(const std::string &string) {
+    Vector<dim> vector;
+    std::stringstream stream(string);
+    for (unsigned int i = 0; i < dim; ++i)
+        stream >> vector[i];
+
+    return vector;
+}
+
+} /* namespace ippp */
+
+#endif    // CONFIGURATOR_H
