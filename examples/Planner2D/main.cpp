b76a38fdb8af8a447acefd08a07bec0d9b455adc
Sascha Kaden <kaden.sascha@googlemail.com>
2017-10-19 11:31:53 +0200

    add map generation to Planner2D example

--- examples/Planner2D/main.cpp
+++ examples/Planner2D/main.cpp
@@ -10,32 +10,50 @@
 #include <modelDirectory.h>
 #include <ui/Drawing2D.hpp>
 #include <ui/ModuleConfigurator.hpp>
+#include <ui/EnvironmentConfigurator.h>
 #include <ui/Writer.hpp>
 
 using namespace ippp;
 
-void testTriangleRobot() {
+Mesh generateMap() {
+    const unsigned int dim = 2;
+    Vector2 min(0, 0);
+    Vector2 max(1000, 1000);
+    std::shared_ptr<Sampler<dim>> sampler(new SamplerRandom<dim>(min, max));
+
+    MapGenerator<dim> mapGenerator(min, max, sampler);
+    auto meshes = mapGenerator.generateMap(40, Vector2(80, 80));
+    auto mesh = cad::mergeMeshes(meshes);
+    cad::exportCad(cad::ExportFormat::OBJ, "obstacle", mesh);
+    return mesh;
+}
+
+bool testTriangleRobot() {
     const unsigned int dim = 3;
+    auto modelDir = getModelDirectory();
+
     Vector3 min(0.0, 0.0, 0.0);
     Vector3 max(1000, 1000, util::twoPi());
 
-    std::vector<Triangle2D> triangles;
-    triangles.push_back(Triangle2D(Vector2(0, 0), Vector2(15, 0), Vector2(15, 15)));
-    triangles.push_back(Triangle2D(Vector2(0, 0), Vector2(0, 15), Vector2(15, 15)));
     ModelFactoryTriangle2D factory;
-    std::shared_ptr<ModelContainer> baseModel = factory.createModel(triangles);
-    std::shared_ptr<TriangleRobot2D> robot(new TriangleRobot2D(baseModel, std::make_pair(min, max)));
-    std::shared_ptr<Environment> environment(new Environment(2, AABB(Vector3(0, 0, 0), Vector3(1000, 1000, 1)), robot));
-    auto workspace = factory.createModels(getModelDirectory() + "/spaces/random2D.obj");
-    environment->addObstacles(workspace);
+    auto model = factory.createModel("obstacle.obj");
+    cad::centerMesh(model->m_mesh);
 
-    std::shared_ptr<CollisionDetection<dim>> collision(new CollisionDetectionTriangleRobot<dim>(environment));
+    EnvironmentConfigurator envConfigurator;
+    envConfigurator.setWorkspaceProperties(dim, AABB(Vector3(0, 0, 0), Vector3(1000, 1000, 1000)));
+    envConfigurator.setRobotType(RobotType::Triangle2D, modelDir + "robots/simpleTriangleRobot.obj");
+    envConfigurator.addObstaclePath("obstacle.obj");
+    envConfigurator.saveConfig("envConfigTriangle.json");
+    std::shared_ptr<Environment> environment = envConfigurator.getEnvironment();
+
     ModuleConfigurator<dim> creator;
     creator.setEnvironment(environment);
+    creator.setGraphSortCount(3000);
     creator.setCollisionType(CollisionType::Dim2Triangle);
-    creator.setEvaluatorType(EvaluatorType::Query);
-    creator.setEvaluatorProperties(10, 10);
-    creator.setSamplingType(SamplingType::NearObstacle);
+    creator.setEvaluatorType(EvaluatorType::QueryOrTime);
+    creator.setEvaluatorProperties(50, 60);
+    creator.setSamplingType(SamplingType::Straight);
+    creator.saveConfig("moduleConfigTriangle.json");
 
     std::shared_ptr<ippp::Planner<dim>> planner;
     //    planner = std::shared_ptr<PRM<dim>>(new PRM<dim>(environment, creator.getPRMOptions(30), creator.getGraph()));
@@ -44,15 +62,15 @@ void testTriangleRobot() {
     // planner = std::shared_ptr<SRT<dim>>(new SRT<dim>(environment, creator.getSRTOptions(20), creator.getGraph()));
 
     auto startTime = std::chrono::system_clock::now();
-    Vector3 start(100, 100, 0);
-    Vector3 goal(800, 300, 50 * util::toRad());
-    bool connected = planner->computePath(start, goal, 1000, 1);
+    Vector3 start(50, 50, 0);
+    Vector3 goal(900, 900, 50 * util::toRad());
+    bool connected = planner->computePath(start, goal, 5000, 3);
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - startTime);
     std::cout << "Computation time: " << std::chrono::milliseconds(duration).count() / 1000.0 << std::endl;
 
     Eigen::MatrixXi workspace2D = cad::create2dspace(environment->getBoundary(), 255);
     std::vector<Mesh> meshes;
-    for (auto obstacle : workspace)
+    for (auto obstacle : environment->getObstacles())
         meshes.push_back(obstacle->m_mesh);
     cad::drawTriangles(workspace2D, meshes, 50);
     cv::Mat image = drawing::eigenToCV(workspace2D);
@@ -61,48 +79,43 @@ void testTriangleRobot() {
     std::vector<std::shared_ptr<Node<dim>>> nodes = planner->getGraphNodes();
     if (connected) {
         Logging::info("Init and goal could be connected!", "Example");
-        std::vector<Vector3> path = planner->getPath(80);
+        std::vector<Vector3> path = planner->getPath(80, 5);
 
-        // Writer::writeVecsToFile(path, "result.txt");
-
-        Eigen::MatrixXi workspace2D = cad::create2dspace(environment->getBoundary(), 255);
-        cv::Mat image = drawing::eigenToCV(workspace2D);
-        // cv::Mat image = obstacleWorkspace.clone();
-        cv::cvtColor(image, image, CV_GRAY2BGR);
-
-        drawing::drawTrianglePath(path, triangles, image, Eigen::Vector3i(0, 0, 255), 2);
+        drawing::drawTrianglePath(path, environment->getRobot()->getBaseModel()->m_mesh, image, Eigen::Vector3i(0, 0, 255), 2);
         writer::writeVecsToFile<3>(path, "test.txt", 1);
 
         cv::namedWindow("pathPlanner", CV_WINDOW_AUTOSIZE);
         cv::imshow("pathPlanner", image);
         cv::imwrite("result.png", image);
         cv::waitKey(0);
+        return true;
     }
+    return false;
 }
 
 void testPointRobot() {
+    const unsigned int dim = 2;
     Vector2 min(0.0, 0.0);
     Vector2 max(1000, 1000);
 
-    const unsigned int dim = 2;
-    std::shared_ptr<PointRobot> robot(new PointRobot(std::make_pair(min, max)));
-    std::shared_ptr<Environment> environment(new Environment(2, AABB(Vector3(0, 0, 0), Vector3(1000, 1000, 1000)), robot));
-
-    ModelFactoryTriangle2D factory;
-    auto workspace = factory.createModels(getModelDirectory() + "/spaces/random2D.obj");
-    environment->addObstacles(workspace);
+    EnvironmentConfigurator envConfigurator;
+    envConfigurator.setWorkspaceProperties(dim, AABB(Vector3(0, 0, 0), Vector3(1000, 1000, 1000)));
+    envConfigurator.addObstaclePath(getModelDirectory() + "/spaces/random2D.obj");
+    envConfigurator.setRobotType(RobotType::Point);
+    envConfigurator.saveConfig("envConfig.json");
+    std::shared_ptr<Environment> environment = envConfigurator.getEnvironment();
 
     ModuleConfigurator<dim> creator;
     creator.setEnvironment(environment);
     creator.setCollisionType(CollisionType::Dim2);
     creator.setGraphSortCount(3000);
     creator.setEvaluatorType(EvaluatorType::QueryOrTime);
-    creator.setEvaluatorProperties(50, 60);
+    creator.setEvaluatorProperties(50, 30);
     creator.setSamplingType(SamplingType::NearObstacle);
     creator.setSamplingProperties(10, 80);
     creator.setMetricWeightVec(Vector2(1.1, 1.1));
-    creator.saveConfig("test.json");
-    creator.loadConfig("test.json");
+    creator.saveConfig("moduleConfig.json");
+    creator.loadConfig("moduleConfig.json");
 
     std::shared_ptr<ippp::Planner<dim>> planner;
     // planner = std::shared_ptr<EST<dim>>(new EST<dim>(environment, creator.getPlannerOptions(), creator.getGraph()));
@@ -122,7 +135,7 @@ void testPointRobot() {
 
     Eigen::MatrixXi workspace2D = cad::create2dspace(environment->getBoundary(), 255);
     std::vector<Mesh> meshes;
-    for (auto obstacle : workspace)
+    for (auto obstacle : environment->getObstacles())
         meshes.push_back(obstacle->m_mesh);
     cad::drawTriangles(workspace2D, meshes, 50);
     cv::Mat image = drawing::eigenToCV(workspace2D);
@@ -145,6 +158,7 @@ void testPointRobot() {
 
 int main(int argc, char** argv) {
     Logging::setLogLevel(LogLevel::debug);
-    //    testTriangleRobot();
+
+    //testTriangleRobot();
     testPointRobot();
 }
